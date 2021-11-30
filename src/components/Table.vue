3c8e71a0447839175172b14b1241444b8cbe0f7b
Eugene-Kuzmenko <e.kuzmenko@dunice.net>
2021-11-30 19:47:32 +0300

    Add table

---
+++ src/components/Table.vue
@@ -0,0 +1,38 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <td v-for="column of columns" :key="column.name">
+          {{ column.name }}
+        </td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row of values" :key="row[keyField]">
+        <td v-for="column of columns" :key="column.name">
+          {{ row[column.field] }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  props: {
+    keyField: {
+      type: String,
+      default: 'id',
+    },
+    values: Array,
+    columns: Array,
+  }
+}
+</script>
+
+<style scoped>
+table {
+  height: fit-content;
+}
+</style>
--- src/routes/FreeFormGraphPage/FreeFormGraphPage.vue
+++ src/routes/FreeFormGraphPage/FreeFormGraphPage.vue
@@ -1,18 +1,39 @@
 <template>
-  <div ref="graph" class="graph">
-
+  <div class="page">
+    <div ref="graph" class="graph" />
+    <div>
+      <Table
+        key-field="nodeId"
+        :columns="priorityQueueColumns"
+        :values="priorityQueue"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { getCurrentInstance, onMounted, onUnmounted } from 'vue';
 import cytoscape from 'cytoscape';
+import Table from '../../components/Table';
 import graphData from '../../graphs/graph.json';
 import graphStyle from './graphStyle';
 
 
 export default {
   name: 'FreeFormGraphPage',
+
+  components: {
+    Table,
+  },
+
+  data: () => ({
+    priorityQueue: [],
+    priorityQueueColumns: [
+      { name: 'Node', field: 'nodeId' },
+      { name: 'Weight', field: 'weight' },
+    ],
+  }),
+
   setup() {
     onMounted(() => {
       const instance = getCurrentInstance();
@@ -38,6 +59,12 @@ export default {
 </script>
 
 <style scoped>
+
+  .page {
+    display: flex;
+    align-content: flex-start;
+  }
+
   .graph {
     position: relative;
     width: 100%;
